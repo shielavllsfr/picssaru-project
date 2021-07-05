3bc6f5dc5d8a416eea0882183fe4a041b3c6dfd2
Shiela Villasfir <shielavillasfir01@gmail.com>
2021-07-05 16:23:32 +0800

    edit profile

--- src/pages/EditProfile.js
+++ src/pages/EditProfile.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, ref } from "react";
+import React, { useState, useEffect, ref, useRef } from "react";
 import firebase from "../utils/firebase";
 import Navigation from "../components/Navigation";
 import PropTypes from "prop-types";
@@ -22,6 +22,9 @@ import {
 } from "@material-ui/core";
 import Favorite from "@material-ui/icons/Favorite";
 import FavoriteBorder from "@material-ui/icons/FavoriteBorder";
+import Snackbar from "@material-ui/core/Snackbar";
+import IconButton from "@material-ui/core/IconButton";
+import CloseIcon from "@material-ui/icons/Close";
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -88,6 +91,167 @@ export default function VerticalTabs() {
     setValue(newValue);
   };
 
+  const db = firebase.firestore();
+  const uid = firebase.auth().currentUser.uid;
+
+  const name = useRef();
+  const username = useRef();
+  const website = useRef();
+  const bio = useRef();
+  const email = useRef();
+  const mobile = useRef();
+  const gender = useRef();
+  const disabled = useRef();
+
+  const [profileExists, setProfileExists] = useState();
+  const [profileID, setProfileId] = useState();
+  const [path, setPath] = useState();
+  const [image, setImage] = useState();
+  const [profileImgChange, setProfileImgChanged] = useState();
+  const [imageURL, setImageURL] = useState();
+
+  const submitHandler = (event) => {
+    let file = image;
+    var storage = firebase.storage();
+    var storageRef = storage.ref();
+
+    const info_changes = {
+      profileURL: imageURL,
+      name: name.current.firstChild.firstChild.value,
+      username: username.current.firstChild.firstChild.value,
+      website: website.current.firstChild.firstChild.value,
+      bio: bio.current.value,
+      email: email.current.firstChild.firstChild.value,
+      mobile: mobile.current.inputRef.value,
+      gender: gender.current.firstChild.firstChild.value,
+      disabled: disabled.current.firstChild.firstChild.checked,
+    };
+
+    if (profileImgChange) {
+      var uploadTask = storageRef
+        .child("profiles/" + uid + " profile")
+        .put(file);
+      uploadTask.on(
+        firebase.storage.TaskEvent.STATE_CHANGED,
+        (snapshot) => {
+          var progress =
+            Math.round(snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log(progress);
+        },
+        (error) => {
+          throw error;
+        },
+        () => {
+          uploadTask.snapshot.ref.getDownloadURL().then((url) => {
+            setImageURL(url);
+            info_changes.profileURL = url;
+            uploadChanges(info_changes);
+          });
+        }
+      );
+    } else {
+      uploadChanges(info_changes);
+    }
+  };
+
+  function uploadChanges(info_changes) {
+    const collection = db
+      .collection("users")
+      .doc(uid)
+      .collection("profile_info");
+    if (profileExists && profileID != null) {
+      collection
+        .doc(profileID)
+        .update({ info_changes })
+        .then((res) => {
+          console.log("updated");
+          setOpenSnack(true);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } else {
+      collection
+        .add({ info_changes })
+        .then((res) => {
+          console.log("saved");
+          console.log(res.id);
+          setProfileId(res.id);
+          setProfileExists(true);
+          setOpenSnack(true);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+    console.log(profileID);
+  }
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        await firebase
+          .firestore()
+          .collection("users")
+          .doc(uid)
+          .collection("profile_info")
+          .get()
+          .then((user_info) => {
+            user_info.forEach((user) => {
+              console.log(user.id);
+              setProfileId(user.id);
+              setProfileExists(true);
+              setPath(user.data().info_changes.profileURL);
+              setImageURL(user.data().info_changes.profileURL);
+              name.current.firstChild.firstChild.value =
+                user.data().info_changes.name;
+              username.current.firstChild.firstChild.value =
+                user.data().info_changes.username;
+              website.current.firstChild.firstChild.value =
+                user.data().info_changes.website;
+              bio.current.value = user.data().info_changes.bio;
+              email.current.firstChild.firstChild.value =
+                user.data().info_changes.email;
+              mobile.current.inputRef.value = user.data().info_changes.mobile;
+              gender.current.firstChild.firstChild.value =
+                user.data().info_changes.gender;
+              disabled.current.firstChild.firstChild.checked =
+                user.data().info_changes.disabled;
+            });
+          });
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
+  }, []);
+
+  const [snack, setOpenSnack] = React.useState(false);
+
+  const handleSnackClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpenSnack(false);
+  };
+
+  const profilePictureHandler = (event) => {
+    if (event.target.files[0] !== undefined) {
+      var file = event.target.files[0];
+      let reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+        setPath(reader.result);
+        setImage(event.target.files[0]);
+        setProfileImgChanged(true);
+      };
+      reader.onerror = function (error) {
+        console.log("Error: ", error);
+      };
+    }
+  };
+
   return (
     <div>
       <Navigation />
@@ -117,13 +281,14 @@ export default function VerticalTabs() {
                 alignItems="center"
                 spacing={2}
               >
-                <Avatar
-                  alt="USERNAME"
-                  src="/img/Shiela.png"
-                  className={classes.large}
-                />
-                <Link href="#" variant="body2">
-                  Change Profie Photo
+                <Avatar alt="USERNAME" src={path} className={classes.large} />
+                <Link href="#" variant="body2" component="label">
+                  <input
+                    hidden
+                    type="file"
+                    onChange={profilePictureHandler}
+                  ></input>
+                  Change Profile Photo
                 </Link>
               </Grid>
 
@@ -135,6 +300,7 @@ export default function VerticalTabs() {
                     variant="filled"
                     size="small"
                     fullWidth
+                    ref={name}
                   />
                 </Grid>
               </Grid>
@@ -155,6 +321,7 @@ export default function VerticalTabs() {
                     variant="outlined"
                     size="small"
                     fullWidth
+                    ref={username}
                   />
                 </Grid>
               </Grid>
@@ -175,6 +342,7 @@ export default function VerticalTabs() {
                     variant="outlined"
                     size="small"
                     fullWidth
+                    ref={website}
                   />
                 </Grid>
               </Grid>
@@ -189,6 +357,7 @@ export default function VerticalTabs() {
                     rowsMax={2}
                     placeholder="Tell me about yourself ❤"
                     maxwidth="100%"
+                    ref={bio}
                   />
                 </Grid>
               </Grid>
@@ -216,6 +385,7 @@ export default function VerticalTabs() {
                     variant="outlined"
                     size="small"
                     fullWidth
+                    ref={email}
                   />
                 </Grid>
               </Grid>
@@ -230,6 +400,7 @@ export default function VerticalTabs() {
                     variant="outlined"
                     type="tel"
                     fullWidth
+                    ref={mobile}
                   />
                 </Grid>
               </Grid>
@@ -243,6 +414,7 @@ export default function VerticalTabs() {
                     variant="outlined"
                     size="small"
                     fullWidth
+                    ref={gender}
                   />
                 </Grid>
 
@@ -256,6 +428,7 @@ export default function VerticalTabs() {
                             checkedIcon={<Favorite />}
                             name="checkedH"
                             color="primary"
+                            ref={disabled}
                           />
                         }
                         label="Temporarily disable my account."
@@ -264,7 +437,11 @@ export default function VerticalTabs() {
 
                     <Grid item container alignItems="center" spacing={2}>
                       <Grid item>
-                        <Button variant="contained" color="primary">
+                        <Button
+                          variant="contained"
+                          color="primary"
+                          onClick={submitHandler}
+                        >
                           SUBMIT
                         </Button>
                       </Grid>
@@ -288,6 +465,29 @@ export default function VerticalTabs() {
           SWITCH ACCOUNT
         </TabPanel>
       </div>
+
+      <Snackbar
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "center",
+        }}
+        open={snack}
+        autoHideDuration={2000}
+        onClose={handleSnackClose}
+        message="SAVED"
+        action={
+          <React.Fragment>
+            <IconButton
+              size="small"
+              aria-label="close"
+              color="inherit"
+              onClick={handleSnackClose}
+            >
+              <CloseIcon fontSize="small" />
+            </IconButton>
+          </React.Fragment>
+        }
+      />
     </div>
   );
 }
