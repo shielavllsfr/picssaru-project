d5f6e0ebc4991b7d344b950fc8c38bba042645b9
Shiela Villasfir <shielavillasfir01@gmail.com>
2021-07-11 17:42:48 +0800

    comments

--- src/pages/Comments.js
+++ src/pages/Comments.js
@@ -1,55 +1,152 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import Divider from "@material-ui/core/Divider";
-import ListItemText from "@material-ui/core/ListItemText";
-import ListItemAvatar from "@material-ui/core/ListItemAvatar";
-import Avatar from "@material-ui/core/Avatar";
-import Typography from "@material-ui/core/Typography";
+import React, { useState, useEffect } from "react";
+import firebase from "../utils/firebase";
 
-const useStyles = makeStyles((theme) => ({
+import {
+  Grid,
+  TextField,
+  Card,
+  CardContent,
+  makeStyles,
+  Button,
+  Typography,
+  IconButton,
+} from "@material-ui/core";
+import DeleteIcon from "@material-ui/icons/Delete";
+
+const useStyle = makeStyles((theme) => ({
   root: {
-    width: "100%",
-    maxWidth: "36ch",
-    backgroundColor: theme.palette.background.paper,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100vh",
+    width: "100vw",
+    direction: "column",
   },
-  inline: {
-    display: "inline",
+  card: {
+    width: 500,
+    marginTop: 20,
+  },
+  taskDone: {
+    textDecoration: "underline",
   },
 }));
 
-function Comments() {
-  const classes = useStyles();
+const db = firebase.firestore();
+
+export default function Dashboard() {
+  const classes = useStyle();
+
+  const [state, setstate] = useState({
+    tasks: [],
+    userUid: "",
+  });
+
+  const [payload, setPayload] = useState({
+    task: "",
+  });
+
+  useEffect(() => {
+    const fetchData = () => {
+      const currentUser = firebase.auth().currentUser;
+
+      db.collection("users")
+        .doc(currentUser.uid)
+        .collection("comments")
+        .orderBy("created_at", "asc")
+        .onSnapshot((doc) => {
+          let taskList = [];
+          doc.forEach((task) => {
+            taskList.push({ ...task.data(), id: task.id });
+          });
+          setstate({
+            tasks: taskList,
+            userUid: currentUser.uid,
+          });
+        });
+    };
+    fetchData();
+  }, []);
+
+  const handleChange = (e) => {
+    setPayload({ task: e.target.value });
+  };
+
+  const addTask = () => {
+    db.collection("users")
+      .doc(state.userUid)
+      .collection("comments")
+      .add({ task: payload.task, created_at: new Date() })
+      .then((docRef) => {
+        //success
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  const deleteTask = (docId) => {
+    db.collection("users")
+      .doc(state.userUid)
+      .collection("comments")
+      .doc(docId)
+      .delete()
+      .then(() => {
+        //success
+      })
+      .catch((err) => {
+        //error
+      });
+  };
 
   return (
-    <List className={classes.root}>
-      <ListItem alignItems="flex-start">
-        <ListItemAvatar>
-          <Avatar
-            alt="Remy Sharp"
-            src="https://images.unsplash.com/photo-1609220361638-14ceb45e5e1e?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max"
-          />
-        </ListItemAvatar>
-        <ListItemText
-          primary="username"
-          secondary={
-            <React.Fragment>
-              <Typography
-                component="span"
-                variant="body2"
-                className={classes.inline}
-                color="textPrimary"
+    <div className="mydiv">
+      <Grid container direction="column" spacing={2} alignItems="center">
+        <Grid item className={classes.card}>
+          <Card>
+            <CardContent>
+              <Grid
+                container
+                direction="column"
+                alignItems="center"
+                justify="center"
               >
-                this is comment
-              </Typography>
-            </React.Fragment>
-          }
-        />
-      </ListItem>
-      <Divider variant="inset" component="li" />
-    </List>
+                {state.tasks.map((task) => (
+                  <React.Fragment key={task.id}>
+                    <Grid container justify="center" alignItems="center">
+                      <Typography
+                        key={task.id}
+                        variant="h6"
+                        component={Button}
+                        className={
+                          task.status === "done" ? classes.taskDone : null
+                        }
+                      >
+                        {task.task}
+                      </Typography>
+                      <IconButton onClick={() => deleteTask(task.id)}>
+                        <DeleteIcon />
+                      </IconButton>
+                    </Grid>
+                  </React.Fragment>
+                ))}
+              </Grid>
+              <Grid container spacing={2} alignItems="center" justify="center">
+                <TextField
+                  variant="outlined"
+                  label="Comments"
+                  value={payload.task}
+                  onChange={handleChange}
+                />
+                <Grid item>
+                  <Button variant="contained" color="primary" onClick={addTask}>
+                    ADD
+                  </Button>
+                </Grid>
+              </Grid>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+    </div>
   );
 }
-
-export default Comments;
